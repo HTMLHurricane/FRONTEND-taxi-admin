247d821e572987eeb5b70a6c2a49c52d09435d3c
IskanderDev01 <githubturabaevi01@gmail.com>
2024-12-23 19:11:41 +0500

    start

--- src/utils/config/axiosInstance.ts
+++ src/utils/config/axiosInstance.ts
@@ -1,46 +1,45 @@
-import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios'
-import { TOKEN } from './token'
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+import { TOKEN } from "./token";
 
-export const baseURL = import.meta.env.VITE_APP_API_URL
+export const baseURL = "https://taxi.aralhub.uz:8000/";
 
-// Расширяем интерфейс AxiosRequestConfig
 interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
-  _retry?: boolean
+  _retry?: boolean;
 }
 
-export const axiosInstance = axios.create({ baseURL })
+export const axiosInstance = axios.create({ baseURL });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = TOKEN.getAccessToken()
-  config.headers['Authorization'] = token ? `Bearer ${token}` : ''
-  return config
-})
+  const token = TOKEN.getAccessToken();
+  config.headers["Authorization"] = token ? `Bearer ${token}` : "";
+  return config;
+});
 
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
-    const originalRequest = error.config as CustomAxiosRequestConfig
+    const originalRequest = error.config as CustomAxiosRequestConfig;
     if (error.response?.status === 401) {
-      originalRequest._retry = true
+      originalRequest._retry = true;
       try {
         // Создаем новый экземпляр axios без интерцепторов для запроса обновления токена
-        const refreshAxios = axios.create({ baseURL })
+        const refreshAxios = axios.create({ baseURL });
         const newToken = await refreshAxios.post<any>(
-          'user/auth/token/refresh/',
+          "user/auth/token/refresh/",
           {
             refresh: TOKEN.getRefreshToken(),
           }
-        )
-        TOKEN.setAccessToken(newToken.data.data.token.access)
+        );
+        TOKEN.setAccessToken(newToken.data.data.token.access);
         originalRequest.headers[
-          'Authorization'
-        ] = `Bearer ${newToken.data.data.token.access}`
-        return axiosInstance(originalRequest)
+          "Authorization"
+        ] = `Bearer ${newToken.data.data.token.access}`;
+        return axiosInstance(originalRequest);
       } catch (refreshError) {
-        TOKEN.removeAccessToken()
-        return Promise.reject(refreshError)
+        TOKEN.removeAccessToken();
+        return Promise.reject(refreshError);
       }
     }
     // return Promise.reject(error)
   }
-)
+);
